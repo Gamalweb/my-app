a770ade092334d9256d1d65ce54aa200902c6d83
gamal <gamal@first>
2023-07-19 18:55:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Welcome from './componants/welcomees6';
+
+function App() {
+  return (
+    <div className='App'>
+  <h1 className='gaa'>gamal</h1>
+    </div>
+  );
+}
+
+export default App;
